bb242aed4c1c6814d8a8c20a007391c49b56e506
YuriSamp <yurisamp123@gmail.com>
2022-12-04 20:22:40 -0300

    update:UI pronta

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,19 +5,21 @@ import logo from '../public/logo1.jpg';
 
 function NavBar() {
   return (
-    <nav className=" items-center justify-center md:flex  md:justify-around py-4 border-b-2 border-black">
-      <div className='md:w-60'>
-      <Image src={logo} alt='Logo da Thrasher' />
+    <nav className=" items-center justify-center md:flex  md:justify-around py-4  shadow-xl">
+      <div className='md:w-32'>
+        <Link href='/'>
+          <Image src={logo} alt='Logo da Thrasher' />
+        </Link>
       </div>
       <ul className='py-2 flex gap-8 items-center justify-center lg:gap-16'>
         <li className='text-center'>
-          <Link href='/' className='text-2xl lg:text-3xl lg:hover:border-b-2 border-black '>Home</Link>
+          <Link href='/' className='text-2xl lg:hover:border-b-2 border-black '>Home</Link>
         </li>
         <li className='text-center'>
-          <Link href='/' className='text-2xl lg:text-3xl lg:hover:border-b-2 border-black '>Shop</Link>
+          <Link href='/' className='text-2xl lg:hover:border-b-2 border-black '>Shop</Link>
         </li>
         <li className='text-center'>
-          <Link href='/' className='text-2xl lg:text-3xl lg:hover:border-b-2 border-black '>Cart</Link>
+          <Link href='/' className='text-2xl lg:hover:border-b-2 border-black '>Cart</Link>
         </li>
       </ul>
     </nav>
